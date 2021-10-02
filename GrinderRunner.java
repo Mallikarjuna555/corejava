ee54ab7d70cb45a45ff5c0226ec5737c62b8a0af
Mallikarjuna5 <mallikarjuna.xworkz@gmail.com>
2021-10-02 14:59:21 +0530

    first commit

---
+++ GrinderRunner.java
@@ -0,0 +1,13 @@
+class GrinderRunner{
+public static void main(String[] args){
+
+Grinder grinder=new Grinder(5,"pegion");
+	System.out.println(grinder.capacity);
+	System.out.println(grinder.company);
+	
+
+Motor motor=grinder.motor;
+	System.out.println(motor.model);
+	System.out.println(motor.rpm);	
+}
+}
